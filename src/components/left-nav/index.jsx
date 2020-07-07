448fa90ae2862f64bf559cad491c22dd2afabb9b
Genp0 <genp0@qq.com>
2020-07-07 19:40:22 +0800

    menuconfig

--- src/components/left-nav/index.jsx
+++ src/components/left-nav/index.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Menu, Icon } from "antd";
 import { Link } from "react-router-dom";
+import menuList from "../../config/menuConfig";
 import logo from "../../assets/images/logo.png";
 import "./index.less";
 const { SubMenu, Item } = Menu;
@@ -8,6 +9,37 @@ const { SubMenu, Item } = Menu;
 左侧导航组建
 */
 export default class LeftNav extends Component {
+  /*
+    根据menu数据数组生成标签数组
+  */
+  getMenuNodes = (menuList) => {
+    return menuList.map((item) => {
+      if (!item.children) {
+        return (
+          <Item key={item.key}>
+            <Link to={item.key}>
+              <Icon type={item.icon} />
+              <span>{item.title} </span>
+            </Link>
+          </Item>
+        );
+      } else {
+        return (
+          <SubMenu
+            key={item.key}
+            title={
+              <span>
+                <Icon type={item.icon} />
+                <span>{item.title}</span>
+              </span>
+            }
+          >
+            {this.getMenuNodes(item.children)}
+          </SubMenu>
+        );
+      }
+    });
+  };
   render() {
     return (
       <div>
@@ -17,7 +49,8 @@ export default class LeftNav extends Component {
             <h1>システム</h1>
           </Link>
           <Menu mode="inline" theme="dark">
-            <Item key="/home">
+            {this.getMenuNodes(menuList)}
+            {/* <Item key="/home">
               <Link to="/home">
                 <Icon type="pie-chart" />
                 <span>首页 </span>
@@ -56,11 +89,9 @@ export default class LeftNav extends Component {
                 <Icon type="pie-chart" />
                 <span>角色管理</span>
               </Link>
-            </Item>
+            </Item> */}
           </Menu>
         </div>
-
-        <Menu></Menu>
       </div>
     );
   }
