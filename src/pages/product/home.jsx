80173ddd3407688b21c9260191c320f3a4e88182
Genp0 <genp0@qq.com>
2020-07-17 21:08:34 +0800

    product detail

---
+++ src/pages/product/home.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Producthome extends Component {
+  render() {
+    return <div>Producthome</div>;
+  }
+}
+
+export default Producthome;
--- src/pages/product/product.jsx
+++ src/pages/product/product.jsx
@@ -1,7 +1,15 @@
 import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
 
+import ProductAddUpdate from "./add-update";
+import ProductDetail from "./detail";
+import ProductHome from "./home";
+
+/*
+Product 的默认子路有组件
+*/
 export default class Product extends Component {
   render() {
-    return <div>Product</div>;
+    return <div>ProductHome</div>;
   }
 }
